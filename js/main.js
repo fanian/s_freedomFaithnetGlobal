175f55663071b2c5e5aee091c0b3b90173c44e32
fanian <rfanian@gmail.com>
2014-08-01 15:17:46 +0400

    initial

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//cycle2 not working properly for styling custom pager, so try cycle first version for pleasure
+
+$(function() {
+
+    $('.slideshow').cycle({
+        timeout:  0,
+        prev:    '.left',
+        next:    '.right',
+        pager:   '#custom-pager'
+    });
+
+
+    });
+
